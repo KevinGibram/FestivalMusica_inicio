1e3750edefa3763bae3eac41cc17f5353b7263a8
KevinGibram <kevinrincon0911@gmail.com>
2023-04-28 22:57:41 -0500

    aplicando watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest} = require("gulp");
+const { src, dest, watch} = require("gulp");
 const sass = require("gulp-sass")(require('sass'));
 
 function css(done) {
@@ -10,4 +10,12 @@ function css(done) {
     done(); // Callback que avisa a gulp cuando llegamos al final
 }
 
+function dev(done){
+
+watch("src/scss/app.scss", css);
+
+done()
+}
+
 exports.css = css;
+exports.dev = dev;
