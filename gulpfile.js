f2501575ed8b2304fb394bfb6dc448cd42dab517
KevinGibram <kevinrincon0911@gmail.com>
2023-05-01 01:14:55 -0500

    Creando un Snippet para las imágenes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-const { src, dest, watch} = require("gulp");
+const { src, dest, watch, parallel} = require("gulp");
+
+//css
 const sass = require("gulp-sass")(require('sass'));
 const plumber =require("gulp-plumber");
+
+
+//imagenes 
+const avif =require("gulp-avif");
+const cache =require("gulp-cache");
+const webp = require("gulp-webp");
+const imagemin = require("gulp-imagemin");
 
 function css(done) {
 
@@ -12,6 +21,38 @@ function css(done) {
     done(); // Callback que avisa a gulp cuando llegamos al final
 }
 
+function imagenes( done){
+    const opcioes = {
+        optimizationLavel: 3
+    }
+    src("src/img/**/*.{png,jpg}")
+    .pipe(cache(imagemin(opcioes)))
+    .pipe(dest("build/img"))
+    done();
+}
+
+function versionWebp( done ){
+    const opciones = {
+        quality: 50
+    };
+
+    src("src/img/**/*.{png,jpg}") //identifica el archivo sass
+    .pipe( webp(opciones))  //compilarlo
+    .pipe( dest("build/img")) //almacenarla en el disco duro
+    done();
+}
+
+function versionAvif( done ){
+    const opciones = {
+        quality: 50
+    };
+
+    src("src/img/**/*.{png,jpg}") //identifica el archivo sass
+    .pipe( avif(opciones))  //compilarlo
+    .pipe( dest("build/img")) //almacenarla en el disco duro
+    done();
+}
+
 function dev(done){
 
 watch("src/scss/**/*.scss", css);
@@ -20,4 +61,7 @@ done()
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
