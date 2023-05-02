7e0a7f9cf5ef6b7914be0294ac6f134c45fc4c6c
KevinGibram <kevinrincon0911@gmail.com>
2023-05-02 00:32:50 -0500

    se agrego la galeria con sus fotos y funciones

---
+++ build/js/app.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function(){ //como van a ser varias funciones las registro con addEventListener y las mando a llamar 
+    iniciarApp();
+});
+
+function iniciarApp(){
+    crearGaleria(); //una funcion especifica para la galeria
+}
+
+function crearGaleria(){
+    const galeria = document.querySelector('.galeria_imagenes');
+
+    for(let i = 1; i <= 12; i++){
+        const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+        <source srcset="src/img/thumb/${i}.avif" type="imag/avif">
+        <source srcset="src/img/thumb/${i}.webp" type="imag/webp">
+        <img width="200" height="300" src="src/img/thumb/${i}.jpg" alt="Imagen de una galeria">
+        `; 
+
+        imagen.onclick = function() {
+            mostarImagen(i);
+        }
+
+        galeria.appendChild(imagen);
+    }
+}
+
+function mostarImagen(id){
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+    <source srcset="src/img/grande/${id}.avif" type="imag/avif">
+    <source srcset="src/img/grande/${id}.webp" type="imag/webp">
+    <img width="200" height="300" src="src/img/grande/${id}.jpg" alt="Imagen de una galeria">
+    `; 
+
+    const overlay = document.createElement("div");
+    overlay.appendChild(imagen);
+    overlay.classList.add("overlay");
+
+    // Agrega un botón para cerrar el overlay
+    const closeButton = document.createElement("button");
+    closeButton.innerHTML = "X";
+    closeButton.addEventListener("click", function() {
+        overlay.remove(); //remove es metodo de js
+    });
+    overlay.appendChild(closeButton);
+
+    const body = document.querySelector("body");
+    body.appendChild(overlay);            
+    body.classList.add("fijar-body");  
+}
--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ function imagenes( done){
     }
     src("src/img/**/*.{png,jpg}")
     .pipe(cache(imagemin(opcioes)))
-    .pipe(dest("build/img"))
+    .pipe(dest("build/img"));
     done();
 }
 
@@ -53,15 +53,24 @@ function versionAvif( done ){
     done();
 }
 
+function javascript(done){
+    src("src/js/*.js")
+    .pipe(dest("build/js"));
+
+    done();
+}
+
 function dev(done){
 
 watch("src/scss/**/*.scss", css);
+watch("src/js/*.js", javascript);
 
 done()
 }
 
 exports.css = css;
+exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
+exports.dev = parallel(imagenes, versionWebp, versionAvif, javascript, dev);
