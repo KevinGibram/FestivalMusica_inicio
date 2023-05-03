4ed69e63215dcec2387d21969ce9119b375b1697
KevinGibram <kevinrincon0911@gmail.com>
2023-05-03 00:42:21 -0500

    Mejorando Código JS con Terser y Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,31 @@ const { src, dest, watch, parallel} = require("gulp");
 //css
 const sass = require("gulp-sass")(require('sass'));
 const plumber =require("gulp-plumber");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
 
 
 //imagenes 
-const avif =require("gulp-avif");
 const cache =require("gulp-cache");
-const webp = require("gulp-webp");
 const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif =require("gulp-avif");
+
+//Javascript
+
+const terser = require("gulp-terser-js");
+
 
 function css(done) {
 
     src("src/scss/**/*.scss") //identifica el archivo sass
+    .pipe( sourcemaps.init())
     .pipe( plumber())
     .pipe( sass())          //compilarlo
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write("."))
     .pipe( dest("build/css")); //almacenarla en el disco duro
 
     done(); // Callback que avisa a gulp cuando llegamos al final
@@ -55,6 +67,9 @@ function versionAvif( done ){
 
 function javascript(done){
     src("src/js/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write("."))
     .pipe(dest("build/js"));
 
     done();
