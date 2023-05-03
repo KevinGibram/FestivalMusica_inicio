fe5eb23aaecced68ec81709344930f6513ddba30
KevinGibram <kevinrincon0911@gmail.com>
2023-05-02 23:08:51 -0500

    Añadiendo un efecto Smooth scrol

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,21 @@ document.addEventListener('DOMContentLoaded', function(){ //como van a ser varia
 
 function iniciarApp(){
     crearGaleria(); //una funcion especifica para la galeria
+    scrollNav();
+}
+
+function scrollNav(){
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+
+    enlaces.forEach( enlace =>{
+        enlace.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView( {behavior: 'smooth'} );
+        });
+    });
 }
 
 function crearGaleria(){
